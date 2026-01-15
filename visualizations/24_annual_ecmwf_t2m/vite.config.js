e17077df45e1613f0c2059c1485f120f1d33bc9c
Lalon Sander <lalon.sander@gmail.com>
2026-01-15 10:40:09 +0100

    update vite config

--- visualizations/24_annual_ecmwf_t2m/vite.config.js
+++ visualizations/24_annual_ecmwf_t2m/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  base: '/taz-klima-globus/visualizations/24_annual_ecmwf_t2m/dist/',
   resolve: {
     alias: {
       '@shared': path.resolve(__dirname, '../../shared'),
     },
+    dedupe: ['three', 'react', 'react-dom', 'react-globe.gl']
   },
   build: {
     outDir: 'dist',
