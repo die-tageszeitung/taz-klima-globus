1ff1b93535cf7569f56624a2da2683f63d089366
Lalon Sander <96609092+LalonSander@users.noreply.github.com>
2026-02-09 10:34:34 +0100

    Update GlobeVisualization.jsx

--- shared/components/GlobeVisualization.jsx
+++ shared/components/GlobeVisualization.jsx
@@ -25,7 +25,7 @@ const GlobeVisualization = ({
     const globe = globeEl.current;
 
     // Auto-rotate
-    globe.controls().autoRotate = true;
+    globe.controls().autoRotate = false;
     globe.controls().autoRotateSpeed = autoRotateSpeed;
 
     // Set beginning coordinates
@@ -36,7 +36,7 @@ const GlobeVisualization = ({
       if (document.hidden) {
         globe.controls().autoRotate = false;
       } else {
-        globe.controls().autoRotate = true;
+        globe.controls().autoRotate = false;
       }
     };
 
