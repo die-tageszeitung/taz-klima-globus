289fc460f2931202a24b31a5c705824acd4cdc7c
Lalon Sander <96609092+LalonSander@users.noreply.github.com>
2026-02-09 09:35:51 +0100

    Update GlobeVisualization.jsx

--- shared/components/GlobeVisualization.jsx
+++ shared/components/GlobeVisualization.jsx
@@ -14,6 +14,7 @@ const GlobeVisualization = ({
   const globeEl = useRef();
   const [countries, setCountries] = useState({ features: [] });
   const [isLoading, setIsLoading] = useState(true);
+  const [hoverD, setHoverD] = useState();
   const [dimensions, setDimensions] = useState({
     width: window.innerWidth,
     height: window.innerHeight * 0.7
